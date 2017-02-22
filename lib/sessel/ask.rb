56d5d4aa3220b1794d624bd0c6084fcbb1344edf
Rafal Nowosielski <rafal@nowosielski.email>
2017-02-22 17:51:51 +0100

    Adding new files

---
+++ lib/sessel/ask.rb
@@ -0,0 +1,69 @@
+module Sessel
+
+  class Ask
+
+    @@cli = HighLine.new
+    @@ses = nil
+    @@s3 = nil
+
+    def self.for_region()
+      region = nil
+      @@cli.choose do |region_menu|
+        region_menu.prompt = "Choose AWS region you want to setup SES in?"
+        region_menu.choices("us-east-1", "us-west-2", "eu-west-1") { |r| region = r }
+      end
+      @@ses = Aws::SES::Client.new(region: region)
+      @@s3 = Aws::S3::Client.new(region: region)
+      return region
+    end
+
+    def self.for_rule_set_name(solution_name)
+      chosen_rule_set_name = nil
+      @@cli.choose do |rule_set_menu|
+        solution_rule_set_name = "#{solution_name}RuleSet"
+        rule_set_menu.prompt = 'Choose receipt rule-set?'
+        resp = @@ses.list_receipt_rule_sets
+        add_new_rule_set = true
+        resp.to_h[:rule_sets].each { |rule_set|
+          rule_set_menu.choice(rule_set[:name]) { chosen_rule_set_name = rule_set[:name] }
+          add_new_rule_set = false if (rule_set[:name] =~ /^#{Regexp.escape(solution_rule_set_name)}$/)
+        }
+        rule_set_menu.choice("Create new: #{solution_rule_set_name}") {
+          chosen_rule_set_name = solution_rule_set_name
+        } if add_new_rule_set
+      end
+      return chosen_rule_set_name
+    end
+
+    def self.for_email_address()
+      return @@cli.ask 'What is the email address you would like to receive emails at?'
+    end
+
+    def self.for_s3_bucket(solution_name)
+      chosen_bucket = nil
+      @@cli.choose do |s3_bucket_menu|
+        solution_bucket_name = solution_name.downcase
+        s3_bucket_menu.prompt = "Choose S3 bucket to put the emails in"
+        resp = @@s3.list_buckets()
+        add_new_bucket = true
+        resp.to_h[:buckets].each { |bucket|
+          s3_bucket_menu.choice(bucket[:name]) { chosen_bucket = bucket[:name] }
+          add_new_bucket = false if (bucket[:name] =~ /^#{Regexp.escape(solution_bucket_name)}$/)
+        }
+        s3_bucket_menu.choice("Create new: #{solution_bucket_name}") {
+          @@s3.create_bucket({
+                                 acl: "private",
+                                 bucket: solution_bucket_name,
+                                 create_bucket_configuration: {
+                                     location_constraint: region
+                                 }
+                             })
+          chosen_bucket = solution_name
+        } if add_new_bucket
+      end
+      return chosen_bucket
+    end
+
+  end
+
+end
--- lib/sessel/receipt_rule.rb
+++ lib/sessel/receipt_rule.rb
@@ -1,6 +1,5 @@
 module Sessel
 
-  # Takes care of creating the receipt rule and making sure all the prerequisites are in place
   class ReceiptRule
 
     def initialize(region, email_addresses, s3_bucket, rule_name, rule_set_name)
@@ -9,109 +8,6 @@ module Sessel
       @s3_bucket = s3_bucket
       @rule_name = rule_name
       @rule_set_name = rule_set_name
-
-      @s3 = Aws::S3::Client.new(region: @region)
-      @sts = Aws::STS::Client.new(region: @region)
-    end
-
-    # Creates the receipt rule in SES accorind to the paramteres passed when the object was initialised
-    def create()
-
-      self.ensure_prerequsites
-
-      ses = Aws::SES::Client.new(region: @region)
-      ses.create_receipt_rule({
-                                  rule: {
-                                      recipients: @email_addresses,
-                                      actions: [
-                                          {
-                                              s3_action: {
-                                                  bucket_name: @s3_bucket,
-                                                  object_key_prefix: 'email',
-                                              },
-                                          },
-                                      ],
-                                      enabled: true,
-                                      name: @rule_name,
-                                      scan_enabled: false,
-                                      tls_policy: 'Optional',
-                                  },
-                                  rule_set_name: @rule_set_name,
-                              })
-    end
-
-    private
-
-    # Makes sure that the chosen bucket has the required policy
-    def ensure_s3_bucket_policy()
-
-      resp = @sts.get_caller_identity({})
-      account = resp.to_h[:account]
-
-      #TODO: Merge existing policies
-      policy = {
-          :Version => "2008-10-17",
-          :Statement => [
-              {
-                  :Sid => "GiveSESPermissionToWriteEmail",
-                  :Effect => "Allow",
-                  :Principal => {
-                      :Service => [
-                          "ses.amazonaws.com"
-                      ]
-                  },
-                  :Action => [
-                      "s3:PutObject"
-                  ],
-                  :Resource => "arn:aws:s3:::#{chosen_bucket}/*",
-                  :Condition => {
-                      :StringEquals => {
-                          'aws:Referer' => account
-                      }
-                  }
-              }
-          ]
-      }
-      puts policy.to_json
-      @s3.put_bucket_policy({
-                                bucket: chosen_bucket,
-                                policy: policy.to_json
-                            })
-    end
-
-    # Ensure the chosen rule set exists
-    def ensure_rule_set()
-      resp = @ses.list_receipt_rule_sets()
-      add_new_rule_set = true
-      resp.to_h[:rule_sets].each { |rule_set|
-        add_new_rule_set = false if (rule_set[:name] =~ /^#{Regexp.escape(@rule_set_name)}$/)
-      }
-      @ses.create_receipt_rule_set({rule_set_name: solution_rule_set_name}) if add_new_rule_set
-    end
-
-    # Ensure the bucket exists
-    def ensure_s3_bucket()
-      resp = @s3.list_buckets()
-      add_new_bucket = true
-      resp.to_h[:buckets].each { |bucket|
-        add_new_bucket = false if (bucket[:name] =~ /^#{Regexp.escape(@s3_bucket)}$/)
-      }
-      @s3.create_bucket({
-                            acl: "private",
-                            bucket: solution_bucket_name,
-                            create_bucket_configuration: {
-                                location_constraint: region
-                            }
-                        }) if add_new_bucket
-    end
-
-    # Execute all the assurances
-    def ensure_prerequsites()
-
-      self.ensure_rule_set
-      self.ensure_s3_bucket
-      self.ensure_s3_bucket_policy
-
     end
 
   end
